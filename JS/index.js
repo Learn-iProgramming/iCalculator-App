46c4be28c70511cc35ea409080ebe51b1807781d
Learn-iProgramming <88945116+Learn-iProgramming@users.noreply.github.com>
2021-12-17 22:42:05 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,23 @@
 let calc = document.getElementById('calc'); 
-btns = document.querySelectorAll('button');
+let btns = document.querySelectorAll('button');
 let calcValue = '';
-for(item of btns){
+for(let item of btns){
     item.addEventListener('click', (e)=>{
         // When click on the button then in button text show in the input box
         btnText = e.target.innerText;
         console.log('Button text is', btnText);
-        // Multiplication
+        // When click this button then its gives multiplication
         if(btnText == 'x'){
             btnText = '*';
             calcValue += btnText;
             calc.value = calcValue;
         }
+        // When click on the Clear button then clear all text in the input 
         else if(btnText == 'C'){
             calcValue = "";
-            calc.value = calcValue;
-            
+            calc.value = calcValue;   
         }
+        // When click on the '=' operator then gives (+, -, *, /, %) answers
         else if(btnText == '='){
             calc.value = eval(calcValue);
         }
